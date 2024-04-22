ba55be9c96bb23966e57f1032db3b48b9aef195e
Cleiton Silva <cleiton_ribeiro@live.com>
2024-04-22 08:20:24 -0300

    arquivos base de configurações

---
+++ Back-end/src/test/java/com/pi5/pi5/Pi5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi5.pi5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
