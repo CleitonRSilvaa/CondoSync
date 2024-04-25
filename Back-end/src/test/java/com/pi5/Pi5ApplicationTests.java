b03bb3d4362e831d0ae95d4488b32d828851b14b
Cleiton Silva <cleiton_ribeiro@live.com>
2024-04-24 21:37:35 -0300

    aquivos de estrutura do projeto

---
+++ Back-end/src/test/java/com/pi5/Pi5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pi5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		// Test if the context loads
+		System.out.println("Context loads");
+	}
+
+}
