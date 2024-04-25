a0659f94c2c5919cc196df22d58a464737d3a300
Cleiton Silva <cleiton_ribeiro@live.com>
2024-04-24 21:39:20 -0300

    feat: implementando spring security

---
+++ Back-end/src/main/java/com/pi5/services/TokenService.java
@@ -0,0 +1,42 @@
+package com.pi5.services;
+
+import com.pi5.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+
+	@Autowired
+	private JwtEncoder encoder;
+
+	public String generateToken(User user, Duration duration) {
+		Instant now = Instant.now();
+
+		String scope = user.getRoles().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.joining(" "));
+
+		JwtClaimsSet claims = JwtClaimsSet.builder()
+				.issuer("self")
+				// .audience(Arrays.asList(clientKey))
+				.issuedAt(now)
+				.expiresAt(now.plus(duration))
+				.subject(user.getUsername())
+				.claim("name", user.getFullName())
+				.claim("scope", scope).build();
+
+		JwtEncoderParameters encoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(),
+				claims);
+		return encoder.encode(encoderParameters).getTokenValue();
+	}
+}
