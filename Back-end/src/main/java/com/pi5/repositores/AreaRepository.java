78ed1b7cce52b931f93b955c37d2549607e5cb1d
Cleiton Silva <cleiton_ribeiro@live.com>
2024-04-26 20:13:01 -0300

    Refactor AreaRepository interface to use UUID as the key type

--- Back-end/src/main/java/com/pi5/repositores/AreaRepository.java
+++ Back-end/src/main/java/com/pi5/repositores/AreaRepository.java
@@ -1,7 +1,10 @@
 package com.pi5.repositores;
 
 import com.pi5.models.Area;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AreaRepository extends JpaRepository<Area, Long> {
+public interface AreaRepository extends JpaRepository<Area, UUID> {
 }
