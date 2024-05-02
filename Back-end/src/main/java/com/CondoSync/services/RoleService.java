20491d4d3653525f429e313dabad5c4320906647
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-02 11:42:07 -0300

    Add RoleService class

---
+++ Back-end/src/main/java/com/CondoSync/services/RoleService.java
@@ -0,0 +1,66 @@
+package com.CondoSync.services;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import com.CondoSync.models.Role;
+import com.CondoSync.repositores.RoleRepository;
+
+@Service
+@Slf4j
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Role getRoleById(Long id) {
+
+        return roleRepository.findById(id).orElseThrow(
+                () -> new ResourceAccessException("Role with id " + id + " not found"));
+    }
+
+    public Role getRoleByNome(String nome) {
+
+        var role = roleRepository.findByNome(nome)
+                .orElseThrow(() -> new ResourceAccessException("Role " + nome + " not found"));
+        return role;
+    }
+
+    public Role createRole(Role role) {
+
+        this.getRoleByNome(role.getNome());
+
+        return roleRepository.save(role);
+    }
+
+    public Role updateRole(Role role) {
+
+        this.getRoleById(role.getId());
+
+        return roleRepository.save(role);
+    }
+
+    public Role patchRole(Role role) {
+
+        this.getRoleById(role.getId());
+
+        return roleRepository.save(role);
+    }
+
+    public void deleteRole(Role role) {
+        this.getRoleById(role.getId());
+
+        roleRepository.delete(role);
+    }
+
+}
