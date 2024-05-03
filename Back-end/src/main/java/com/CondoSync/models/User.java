0c14fc8a49c7e3fc13a18253cdca08d68b118e12
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-03 20:24:19 -0300

    feat: Add lazy loading for user subscriptions in User model

--- Back-end/src/main/java/com/CondoSync/models/User.java
+++ Back-end/src/main/java/com/CondoSync/models/User.java
@@ -7,10 +7,15 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -35,6 +40,7 @@ public class User implements UserDetails {
 
     @NonNull
     @Column(length = 500, nullable = false)
+    @JsonIgnore
     private String hashPassword;
 
     private LocalDateTime datahashSenhaUpdate;
@@ -42,6 +48,10 @@ public class User implements UserDetails {
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<PushSubscription> subscriptions = new HashSet<>();
 
     @CreationTimestamp
     private Instant creation;
@@ -54,13 +64,9 @@ public class User implements UserDetails {
     private boolean inativa;
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.roles;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.hashPassword;
     }
 
     @Override
@@ -69,18 +75,14 @@ public class User implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return false;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
-
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
 
         return false;
     }
@@ -88,6 +90,27 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return this.status;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+        return this.hashPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User [creation=" + creation + ", datahashSenhaUpdate=" + datahashSenhaUpdate + ", fullName=" + fullName
+                + ", hashPassword=" + hashPassword + ", id=" + id + ", inativa=" + inativa + ", roles=" + roles.size()
+                + ", status="
+                + status + ", subscriptions=" + subscriptions.size() + ", upudate=" + upudate + ", userName=" + userName
+                + "]";
     }
 
 }
