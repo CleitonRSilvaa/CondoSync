6f347eab4d7878327ca851e8abc72838e0858273
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-03 20:25:43 -0300

    chore: Add lazy loading for images in ImageRepository

--- Back-end/src/main/java/com/CondoSync/repositores/ImageRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/ImageRepository.java
@@ -1,8 +1,10 @@
 package com.CondoSync.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.CondoSync.models.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
 }
