11c3127165e931892274d9c342728f201320e735
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-05 01:24:08 -0300

    🦄 refactor(App.js):  service worker registration and subscription handling

--- Front-end/app.js
+++ Front-end/app.js
@@ -1,84 +1,131 @@
-<<<<<<< HEAD
 const baseUrl = "http://localhost:8020/api/v1/notifications";
 
 const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-
-            const permission = await Notification.requestPermission();
-            if (permission !== "granted") {
-               return;
-            }
-
-            const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
-            const publicVapidKey = await getPublicKey();
-            let subscription = await registration.pushManager.getSubscription();
-            if (!subscription) {
-                subscription = await registration.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-                });
-                await saveSubscribe(subscription);
-            } 
-        } catch (error) {
-            console.error(`Service Worker registration failed with ${error}`);
-        }
-    }
-};
-async function getPublicKey() {
+  if ("serviceWorker" in navigator) {
     try {
-        let url = baseUrl + "/public-key";
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/text',
-                'Content-Type': 'application/text'                
-            }
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        return;
+      }
+
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      const publicVapidKey = await getPublicKey();
+      let subscription = await registration.pushManager.getSubscription();
+      if (!subscription) {
+        subscription = await registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
         });
-        if (response.ok) {
-            return await response.text();
-        }
-        return null;
+        await saveSubscribe(subscription);
+      }
     } catch (error) {
-        console.log('Error getting key from server:', error);
-        return null;
+      console.error(`Service Worker registration failed with ${error}`);
     }
+  }
+};
+
+const saveServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        return;
+      }
+
+      const registration = await navigator.serviceWorker.getRegistration(
+        "/sw.js",
+        {
+          scope: "/",
+        }
+      );
+
+      const publicVapidKey = await getPublicKey();
+      let subscription = await registration.pushManager.getSubscription();
+      if (subscription) {
+        console.log("Subscription already exists:", subscription);
+        await saveSubscribe(subscription);
+      }
+    } catch (error) {
+      console.error(`Service Worker registration failed with ${error}`);
+    }
+  }
+};
+
+async function getPublicKey() {
+  try {
+    let url = baseUrl + "/public-key";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/text",
+        "Content-Type": "application/text",
+      },
+    });
+    if (response.ok) {
+      return await response.text();
+    }
+    return null;
+  } catch (error) {
+    console.log("Error getting key from server:", error);
+    return null;
+  }
 }
 
 async function saveSubscribe(subscription) {
-    try {
-        let url = baseUrl + "/subscribe";
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'                
-            },
-            body: JSON.stringify(subscription)
-        });
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Subscription saved:', data);
-            return data;
-        }
-        return null;
-    } catch (error) {
-        console.log('Error saving subscription:', error);
-        return null;
+  console.log("para salvar ---->", JSON.stringify(subscription));
+
+  try {
+    let url = baseUrl + "/subscribe";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(subscription),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Subscription saved:", data);
+      return data;
     }
+    return null;
+  } catch (error) {
+    console.log("Error saving subscription:", error);
+    return null;
+  }
 }
 
 function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
-registerServiceWorker(); 
+function showNotification() {
+  title = "Push Notification";
+  body = "Você tem uma nova notificação.";
+  icon = "images/icon.png";
+  data = {
+    dateOfArrival: Date.now(),
+    primaryKey: "1",
+  };
+
+  const options = {
+    body,
+    icon,
+    data,
+  };
+  new Notification(title, options);
+}
+
+// registerServiceWorker();
