85612a2f85acd1915b4a5465786fe01b9eaea424
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-05 01:27:42 -0300

    feat: Add user subscription deletion endpoint
    This commit adds an `unsubscribe` endpoint to the `PushManagerController` class, allowing users to unsubscribe from push notifications. The endpoint handles the deletion of user subscriptions and returns a `ResponseEntity` as a response.

--- Back-end/src/main/java/com/CondoSync/services/UserSubscriptionService.java
+++ Back-end/src/main/java/com/CondoSync/services/UserSubscriptionService.java
@@ -1,5 +1,10 @@
 package com.CondoSync.services;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,7 +12,9 @@ import org.springframework.stereotype.Service;
 
 import com.CondoSync.models.PushSubscription;
 import com.CondoSync.models.User;
+import com.CondoSync.models.DTOs.SubscripitionDTO;
 import com.CondoSync.models.DTOs.UserSubscriptionDTO;
+import com.CondoSync.models.DTOs.SubscripitionDTO.Keys;
 import com.CondoSync.repositores.UserSubscriptionRepository;
 
 @Service
@@ -31,10 +38,32 @@ public class UserSubscriptionService {
         subscribe.setP256dh(userSubscriptionDTO.getKeys().getP256dh());
         subscribe.setAuth(userSubscriptionDTO.getKeys().getAuth());
         subscribe.setUser(user);
-        // subscribe.setHashKey(subscribe.getHashKey());
+
         userSubscriptionRepository.save(subscribe);
 
         return ResponseEntity.status(201).build();
+    }
+
+    public ResponseEntity<?> deleteSubscription(UserSubscriptionDTO userSubscriptionDTO) {
+        return null;
+    }
+
+    public List<?> getSubscriptions(UUID userId) {
+
+        List<SubscripitionDTO> subs = new ArrayList<>();
+
+        var subscriptions = (List<PushSubscription>) userSubscriptionRepository.findAllByUserId(userId);
+
+        for (PushSubscription pushSubscription : subscriptions) {
+            SubscripitionDTO sub = new SubscripitionDTO();
+            sub.setEndpoint(pushSubscription.getEndpoint());
+            sub.setExpirationTime(pushSubscription.getExpirationTime());
+            sub.setKeys(new Keys(pushSubscription.getP256dh(), pushSubscription.getAuth()));
+            subs.add(sub);
+        }
+
+        return subs;
+
     }
 
 }
