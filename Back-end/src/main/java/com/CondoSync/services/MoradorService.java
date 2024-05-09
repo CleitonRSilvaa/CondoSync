7d28094b576649cd60ff5659f9402837eba95679
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-09 20:46:04 -0300

    refactor: Update MoradorService to handle morador-related operations
    This commit adds the MoradorService class to the Back-end/src/main/java/com/CondoSync/services directory. The MoradorService class provides methods for finding a morador by CPF or email, as well as saving a new morador. It uses the MoradorRepository to interact with the database. This service will be used to handle morador-related operations in the application.

---
+++ Back-end/src/main/java/com/CondoSync/services/MoradorService.java
@@ -0,0 +1,29 @@
+package com.CondoSync.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.CondoSync.models.Morador;
+import com.CondoSync.repositores.MoradorRepository;
+
+@Service
+public class MoradorService {
+
+    @Autowired
+    private MoradorRepository moradorRepository;
+
+    public Optional<Morador> findByCpf(String cpf) {
+        return moradorRepository.findByCpf(cpf);
+    }
+
+    public Optional<Morador> findByEmail(String email) {
+        return moradorRepository.findByEmail(email);
+    }
+
+    public Morador save(Morador morador) {
+        return moradorRepository.save(morador);
+    }
+
+}
