1dfec06da9a71f07412ebdd706c79a8f847969c3
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-10 21:25:04 -0300

    feat: Add user registration endpoint to MoradorController
    This commit adds a new endpoint `/register` to the `MoradorController` class in the `Back-end/src/main/java/com/CondoSync/controllers/MoradorController.java` file. The endpoint handles the registration of a new user by accepting a `MoradorDTO` object in the request body. The `MoradorService` is used to register the user, and if successful, the created `MoradorDTO` is returned with a `201 Created` status. If the registration fails, a `400 Bad Request` status is returned. This addition enhances the functionality of the application by allowing users to register and join the system.

--- Back-end/src/main/java/com/CondoSync/controllers/MoradorController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/MoradorController.java
@@ -3,18 +3,37 @@ package com.CondoSync.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.CondoSync.models.User;
+import com.CondoSync.models.DTOs.MoradorDTO;
 import com.CondoSync.services.MoradorService;
 import com.CondoSync.services.RoleService;
 import com.CondoSync.services.UserService;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/v1/morador")
+@Validated
 public class MoradorController {
 
     @Autowired
     private MoradorService moradorService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid MoradorDTO moradorDTO) {
+
+        var morador = moradorService.register(moradorDTO);
+        if (morador == null) {
+            return new ResponseEntity<>(400, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(moradorDTO, HttpStatus.CREATED);
+    }
 
 }
