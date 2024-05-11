c00a8138aa00034fbfb8471871f6efafa0d2ea4e
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-10 21:37:36 -0300

    feat: Add files js

---
+++ Back-end/src/main/resources/static/js/main.js
@@ -0,0 +1,177 @@
+(function($) {
+	"use strict";
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+	$(".toggle-password").click(function() {
+
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
+})(jQuery);
+
+document
+  .getElementById("forms-login")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+	const btn = document.getElementById("btn-login");
+    const loading = document.getElementById("loading");
+    btn.disabled = true;
+    loading.style.display = 'block'; 
+
+
+
+    clearError();
+
+	const email = document.getElementById("email");
+	const senha = document.getElementById("password-field");
+
+	if (email.value.trim() === "" || senha.value.trim() === "") {
+		if (email.value.trim() === "") {
+			getOrCreateErrorMsg(email, "O email é obrigatório.");
+		}
+		if (senha.value.trim() === "") {
+			getOrCreateErrorMsg(senha, "A senha é obrigatória.");
+		}
+	} else {
+	   await postLogin(email.value, senha.value);
+	}
+  });
+
+
+  async function postLogin(email, senha) {
+	const body = {
+	  email: email,
+	  password: senha,
+	};
+	try {
+
+	  	const response = await fetch("http://localhost:8010/api/v1/login", {
+			method: "POST",
+			headers: {
+		  		"Content-Type": "application/json",
+			},
+			credentials: 'include',  
+			body: JSON.stringify(body),
+	 	});
+  
+		if (response.ok) {
+			const data = await response.json();
+			localStorage.setItem('token',data.token); 
+			window.location.href = "http://localhost:8011/Perfil/perfil.html";
+		}
+  
+	  	if (response.status === 401) {
+			const data = await response.json();
+			$('#divPaiForms').empty();
+			const divPaiForms = document.getElementById("divPaiForms");
+			divPaiForms.insertAdjacentHTML(
+				"afterbegin",
+				criarMensagemDeErro(data.message, data.error)
+			);
+			return;
+		
+		}if (response.status >= 400 && response.status < 500) {
+			const data = await response.json();
+			$('#divPaiForms').empty();
+			const divPaiForms = document.getElementById("divPaiForms");
+			divPaiForms.insertAdjacentHTML(
+				"afterbegin",
+				criarMensagemDeErro(data.message, data.error)
+			);
+			return;
+
+		}if (response.status === 500) {
+			$('#divPaiForms').empty();
+			const divPaiForms = document.getElementById("divPaiForms");
+			divPaiForms.insertAdjacentHTML(
+				"afterbegin",
+				criarMensagemDeErro("Erro ao fazer login, tente novamente mais tarde!")
+			);
+			return;
+		}
+		
+	} catch (error) {
+		$('#divPaiForms').empty();
+
+	  console.error("Erro ao enviar a requisição:", error);
+	  const divPaiForms = document.getElementById("divPaiForms");
+	  divPaiForms.insertAdjacentHTML(
+		"afterbegin",
+		criarMensagemDeErro("Erro ao fazer login, tente novamente mais tarde!")
+	  );
+	  return;
+	}finally
+	{
+		const bnt = document.getElementById("btn-login");
+		const loading = document.getElementById("loading");
+		loading.style.display = 'none';
+		bnt.disabled = false;
+	}
+}
+  
+  function criarMensagemDeErro(titulo ="",texto) {
+	return `
+	<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>${titulo}</strong> <p>${texto}</p>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+	`;
+  }
+
+  function clearError() {
+	let formulario = document.getElementById("forms-login");
+	if (formulario) {
+	  for (let i = 0; i < formulario.elements.length; i++) {
+		let campo = formulario.elements[i];
+		if (campo.tagName === "INPUT") {
+			if (campo.classList.contains("border-danger")) {
+				campo.classList.remove("border-danger");
+			}
+			if (!campo.classList.contains("border-dark")) {
+				campo.classList.add("border-dark");
+			}
+
+		  let msgErro = campo.nextElementSibling;
+		  if (msgErro && msgErro.classList.contains("mensagem-erro")) {
+			campo.parentNode.removeChild(msgErro);
+		  }
+		}
+	  }
+	}
+  }
+  
+  function getOrCreateErrorMsg(input, message) {
+	input.classList.add("border-danger");
+	input.classList.remove(" border-dark");
+	let msgErro = input.nextElementSibling;
+	if (!msgErro || !msgErro.classList.contains("mensagem-erro")) {
+	  msgErro = document.createElement("p");
+	  msgErro.classList.add(
+		"mt-2",
+		"text-sm",
+		"text-red-600",
+		"dark:text-red-500",
+		"mensagem-erro"
+	  );
+	  msgErro.innerHTML = `<span class="font-medium"></span> ${message}`;
+	  input.parentNode.appendChild(msgErro);
+	}
+  }
+  
+
---
+++ Back-end/src/main/resources/static/sw.js
@@ -0,0 +1,72 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        self.skipWaiting()
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        self.clients.claim()
+    );
+});
+
+self.addEventListener('push', function(event) {
+    const getData = async (event) => {
+
+        if(!(self.Notification && self.Notification.permission === 'granted')) {
+            return;
+        }
+
+        try {
+            if (event.data) {
+                let data = {};
+                try {
+                    data = event.data.json(); 
+                } catch (error) {
+                    data = { title: 'Push Notification', body: event.data.text(), icon: 'images/icon.png', primaryKey: '1' };
+                }
+
+                const options = {
+                    body: data.body || 'Você tem uma nova notificação.',
+                    icon: data.icon || 'images/icon.png',
+                    vibrate: [100, 50, 100],
+                    data: {
+                        dateOfArrival: Date.now(),
+                        primaryKey: data.primaryKey || '1'
+                    }
+                };
+
+                event.waitUntil(
+                    self.registration.showNotification(data.title || 'Push Notification', options)
+                );
+            } else {
+                const fallbackOptions = {
+                    body: 'Você tem uma nova notificação.',
+                    icon: 'images/icon.png',
+                    vibrate: [100, 50, 100],
+                    data: {
+                        dateOfArrival: Date.now(),
+                        primaryKey: '2'
+                    }
+                };
+                event.waitUntil(
+                    self.registration.showNotification('Push Notification', fallbackOptions)
+                );
+            }
+        } catch (error) {
+            console.error('Error handling push event:', error);
+        }
+    };
+
+    getData(event);
+});
+
+
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('https://example.com')
+    );
+
+});
