2339104150d2c261b89e2d6dc1d507d0228cbf05
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-10 21:21:14 -0300

    refactor: Update UserService to include password encoding
    This commit adds the PasswordEncoder dependency to the UserService class in the Back-end/src/main/java/com/CondoSync/services/UserService.java file. The PasswordEncoder will be used to encode passwords before storing them in the database. This change improves security by ensuring that passwords are not stored in plain text.

--- Back-end/src/main/java/com/CondoSync/services/UserService.java
+++ Back-end/src/main/java/com/CondoSync/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.CondoSync.models.User;
@@ -18,6 +19,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UsersRepository usersRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -58,6 +62,10 @@ public class UserService implements UserDetailsService {
 
     public Optional<User> findById(UUID id) {
         return usersRepository.findById(id);
+    }
+
+    public String encodePassword(String senha) {
+        return passwordEncoder.encode(senha);
     }
 
 }
