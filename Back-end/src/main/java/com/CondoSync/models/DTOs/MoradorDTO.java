9a71bcba772ba330f0907020eae9a09fe0c34d4b
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-10 21:23:53 -0300

    refactor: Update MoradorDTO class with validation annotations and additional methods
    This commit adds validation annotations to the MoradorDTO class in the Back-end/src/main/java/com/CondoSync/models/DTOs/MoradorDTO.java file. The @CPF annotation is added to validate the CPF field, ensuring it is a valid Brazilian CPF number. Additionally, the @NotBlank and @Size annotations are used to enforce constraints on the nomeCompleto, senha, and confirmacaoSenha fields.
    
    Two new methods, addRoleId and validateRolesIds, are added to the MoradorDTO class. The addRoleId method allows adding a role ID to the rolesIds set, ensuring the ID is not null and greater than zero. The validateRolesIds method checks if any role ID in the rolesIds set is null or less than one, throwing an exception if any invalid ID is found.
    
    These changes improve the data integrity and validation in the MoradorDTO class, ensuring that the required fields are not empty and the role IDs are valid.

--- Back-end/src/main/java/com/CondoSync/models/DTOs/MoradorDTO.java
+++ Back-end/src/main/java/com/CondoSync/models/DTOs/MoradorDTO.java
@@ -1,9 +1,18 @@
 package com.CondoSync.models.DTOs;
 
 import java.util.List;
+import java.util.Set;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class MoradorDTO {
 
+    @CPF(message = "O CPF deve ser valido")
     @NotBlank(message = "O CPF é obrigatorio")
     @Size(min = 11, max = 11, message = "O CPF deve ter 11 caracteres")
     private String cpf;
@@ -31,32 +41,41 @@ public class MoradorDTO {
     @Size(min = 5, max = 100, message = "O email deve ter entre 5 e 100 caracteres")
     private String email;
 
-    @NotBlank(message = "O nome é obrigatorio")
-    @Size(min = 5, max = 100, message = "O nome deve ter entre 5 e 100 caracteres")
-    private String nome;
+    @NotBlank(message = "O nome completo é obrigatorio")
+    @Size(min = 5, max = 100, message = "O nome completo deve ter entre 5 e 100 caracteres")
+    private String nomeCompleto;
 
     @NotBlank(message = "A senha é obrigatorio")
     @Size(min = 5, max = 100, message = "A senha deve ter entre 5 e 100 caracteres")
     private String senha;
 
-    private List<Integer> rolesIds;
+    @NotBlank(message = "A confirmação de senha é obrigatorio")
+    @Size(min = 5, max = 100, message = "A confirmação de senha deve ter entre 5 e 100 caracteres")
+    private String confirmacaoSenha;
 
-    public void setRolesIds(List<Integer> rolesIds) {
-        for (Integer id : rolesIds) {
-            if (id != null && id >= 0) {
-                this.rolesIds.add(id);
-            } else {
-                throw new IllegalArgumentException("Role ID não pode ser negativo.");
-            }
-        }
-    }
+    @NotEmpty(message = "rolesIds não pode ser nulo ou vazio.")
+    private Set<Long> rolesIds;
 
-    public void addRoleId(Integer roleId) {
-        if (roleId != null && roleId >= 0) {
-            rolesIds.add(roleId);
+    public void addRoleId(Long roleId) {
+        if (roleId != null && roleId > 0) {
+            this.rolesIds.add(roleId);
         } else {
             throw new IllegalArgumentException("Role ID não pode ser negativo.");
         }
+    }
+
+    public void validateRolesIds() {
+        for (Long id : this.rolesIds) {
+            if (id == null || id < 1) {
+                throw new IllegalArgumentException("Role ID deve ser positivo e maior que zero.");
+            }
+
+        }
+    }
+
+    @JsonIgnore
+    public boolean isSenhaValida() {
+        return this.senha.equals(this.confirmacaoSenha);
     }
 
 }
