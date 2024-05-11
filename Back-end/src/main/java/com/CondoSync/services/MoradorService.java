be2817fd577fe88156960bf99f50106f91613c2b
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-10 21:24:13 -0300

    refactor: Update MoradorService to include user registration logic

--- Back-end/src/main/java/com/CondoSync/services/MoradorService.java
+++ Back-end/src/main/java/com/CondoSync/services/MoradorService.java
@@ -1,11 +1,21 @@
 package com.CondoSync.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.CondoSync.components.ValidateUserException;
 import com.CondoSync.models.Morador;
+import com.CondoSync.models.Role;
+import com.CondoSync.models.User;
+import com.CondoSync.models.DTOs.MoradorDTO;
 import com.CondoSync.repositores.MoradorRepository;
 
 @Service
@@ -29,6 +39,52 @@ public class MoradorService {
     }
 
     public Morador save(Morador morador) {
+        return moradorRepository.save(morador);
+    }
+
+    public Morador register(MoradorDTO moradorDTO) {
+
+        moradorDTO.validateRolesIds();
+        if (!moradorDTO.isSenhaValida()) {
+            throw new IllegalArgumentException("As senhas não conferem");
+        }
+
+        HashMap<String, Object> dHashMap = new HashMap<>();
+
+        if (findByCpf(moradorDTO.getCpf()).isPresent()) {
+            dHashMap.put("CPF", "CPF já cadastrado");
+        }
+        if (findByEmail(moradorDTO.getEmail()).isPresent() || userService.existsByUserName(moradorDTO.getEmail())) {
+            dHashMap.put("Email", "Email já cadastrado");
+        }
+
+        if (!dHashMap.isEmpty()) {
+            throw new ValidateUserException("Um registro com os mesmos dados já existe.", dHashMap);
+        }
+
+        Morador morador = new Morador();
+        User user = new User();
+        List<Role> roles = new ArrayList<Role>();
+
+        for (Long id : moradorDTO.getRolesIds()) {
+            roles.add(roleService.getRoleById(id));
+        }
+
+        user.setFullName(moradorDTO.getNomeCompleto());
+        user.setUserName(moradorDTO.getEmail());
+        user.setHashPassword(userService.encodePassword(moradorDTO.getSenha()));
+        user.setStatus(true);
+        user.setInativa(false);
+        user.setRoles(roles);
+        userService.createUser(user);
+
+        morador.setCpf(moradorDTO.getCpf());
+        morador.setBloco(moradorDTO.getBloco());
+        morador.setApartamento(moradorDTO.getApartamento());
+        morador.setCelular(moradorDTO.getCelular());
+        morador.setEmail(moradorDTO.getEmail());
+        morador.setNome(moradorDTO.getNomeCompleto());
+        morador.setUser(user);
         return moradorRepository.save(morador);
     }
 
