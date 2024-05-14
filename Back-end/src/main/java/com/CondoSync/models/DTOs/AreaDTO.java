c0a191cf36a4784bc1a72887c0aa5a4cafed9709
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-14 13:34:36 -0300

    feat: Add AreaDTO model for data transfer
    A new model class `AreaDTO` has been added in the `AreaDTO.java` file. This model is used for data transfer and contains fields such as `name`, `description`, `price`, and `images`. The `name`, `description`, and `price` fields have validation annotations to ensure data integrity. This addition enhances the application's data handling capabilities and improves the organization of data transfer objects.

---
+++ Back-end/src/main/java/com/CondoSync/models/DTOs/AreaDTO.java
@@ -0,0 +1,36 @@
+package com.CondoSync.models.DTOs;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AreaDTO {
+
+    @NotBlank(message = "A nome é obrigatorio")
+    @Size(min = 5, max = 100, message = "O nome deve ter entre 5 e 100 caracteres")
+    private String name;
+
+    @NotBlank(message = "A descrição é obrigatoria")
+    @Size(min = 5, max = 100, message = "A descrição deve ter entre 5 e 100 caracteres")
+    private String description;
+
+    @NotNull(message = "O preço é obrigatorio")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero")
+    private BigDecimal price;
+
+    private List<Image> images;
+
+    public record Image(String path, String name) {
+    }
+}
