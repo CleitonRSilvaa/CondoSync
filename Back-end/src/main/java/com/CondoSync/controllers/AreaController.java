1df079e813c0e7cb8256f530d93390003ad8ba4b
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-15 21:07:08 -0300

    feat: Add HorarioService, HorarioRepository, and HorarioDTO for managing schedules

--- Back-end/src/main/java/com/CondoSync/controllers/AreaController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/AreaController.java
@@ -1,6 +1,6 @@
 package com.CondoSync.controllers;
 
-import javax.swing.text.html.parser.Entity;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.CondoSync.models.Area;
 import com.CondoSync.models.DTOs.AreaDTO;
+import com.CondoSync.models.DTOs.HorarioDTO;
+import com.CondoSync.models.DTOs.ReservaMoradorDTO;
 import com.CondoSync.services.AreaService;
+import com.CondoSync.services.HorarioService;
 
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -25,6 +28,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AreaController {
     @Autowired
     private AreaService areaService;
+
+    @Autowired
+    private HorarioService horarioService;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody @Valid AreaDTO areaDto) {
@@ -39,6 +45,37 @@ public class AreaController {
     @GetMapping("/list")
     public ResponseEntity<?> listAll() {
         return new ResponseEntity<>(areaService.listAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("schedule/register")
+    public ResponseEntity<?> registerHorario(@RequestBody @Valid HorarioDTO horarioDto) {
+        return new ResponseEntity<>(horarioService.register(horarioDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("schedule/list")
+    public ResponseEntity<?> listAllHorarios() {
+        return new ResponseEntity<>(horarioService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("schedule/find")
+    public ResponseEntity<?> findHorario(@RequestParam Integer id) {
+        return new ResponseEntity<>(horarioService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("{areaId}/schedule")
+    public ResponseEntity<?> findHorarioByArea(@PathVariable UUID areaId) {
+        return new ResponseEntity<>(horarioService.findByAreaId(areaId), HttpStatus.OK);
+    }
+
+    @GetMapping("{areaId}/schedules")
+    public ResponseEntity<?> findAllHorariosByArea(@PathVariable UUID areaId) {
+        return new ResponseEntity<>(horarioService.findAllByAreaId(areaId), HttpStatus.OK);
+    }
+
+    @PostMapping("/reservation")
+    public ResponseEntity<?> reserve(@RequestBody @Valid ReservaMoradorDTO reservaMoradorDTO) {
+
+        return new ResponseEntity<>(areaService.reservarArea(reservaMoradorDTO), HttpStatus.CREATED);
     }
 
 }
