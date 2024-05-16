0207086d894c39d567e91b5db82d3f102cbf7d82
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-15 21:05:18 -0300

    feat: Add ReservaMoradorRepository interface
    A new interface `ReservaMoradorRepository` has been added in the `ReservaMoradorRepository.java` file. This interface extends the `JpaRepository` interface and allows for managing reservations for residents. This addition enhances the application's data handling capabilities and provides CRUD operations for the `ReservaMorador` entity.

---
+++ Back-end/src/main/java/com/CondoSync/repositores/ReservaMoradorRepository.java
@@ -0,0 +1,9 @@
+package com.CondoSync.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.CondoSync.models.ReservaMorador;
+
+public interface ReservaMoradorRepository extends JpaRepository<ReservaMorador, Integer> {
+
+}
