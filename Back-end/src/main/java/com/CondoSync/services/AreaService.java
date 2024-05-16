74ad40a45b66d7e6251fcaba00d4498c5fadb49a
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-15 21:06:13 -0300

    feat: Add reservation functionality for residents
    The code changes include adding a new model class `ReservaMoradorDTO` and repository interfaces `ReservaAreaRepository` and `ReservaMoradorRepository`. These additions enhance the application's data handling capabilities and allow for managing reservations for residents. Additionally, the `Morador` model has been updated to include lazy loading for images, improving page load performance. The `Horario` model has been updated with validation annotations and a `toString` method, enhancing data handling and debugging capabilities. Lastly, the `StatusReserva` enum has been added to manage reservation statuses. These changes provide improved functionality for managing reservations and enhance the overall performance of the application.

--- Back-end/src/main/java/com/CondoSync/services/AreaService.java
+++ Back-end/src/main/java/com/CondoSync/services/AreaService.java
@@ -2,21 +2,42 @@ package com.CondoSync.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.health.HealthProperties.Status;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.CondoSync.models.Image;
+import com.CondoSync.models.Reserva;
+import com.CondoSync.models.ReservaMorador;
+import com.CondoSync.models.StatusReserva;
 import com.CondoSync.models.Area;
 import com.CondoSync.models.DTOs.AreaDTO;
+import com.CondoSync.models.DTOs.ReservaMoradorDTO;
 import com.CondoSync.repositores.AreaRepository;
+import com.CondoSync.repositores.ReservaAreaRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
 @Service
 public class AreaService {
     @Autowired
     private AreaRepository areaRepository;
+
+    @Autowired
+    private HorarioService horarioService;
+
+    @Autowired
+    private MoradorService moradorService;
+
+    @Autowired
+    private ReservaAreaRepository reservaAreaRepository;
+
+    @Autowired
+    private ReservaMoradorService reservaMoradorService;
 
     public Area register(@Valid AreaDTO areaDTO) {
 
@@ -54,6 +75,54 @@ public class AreaService {
             areas.add(areaDTO);
         }
         return areas;
+    }
+
+    public Area findById(UUID id) {
+        return areaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Area with id " + id + " not found"));
+    }
+
+    public ResponseEntity<?> reservarArea(ReservaMoradorDTO reservaMoradorDTO) {
+
+        var area = findById(reservaMoradorDTO.getAreaId());
+
+        var horario = horarioService.findById(reservaMoradorDTO.getHorarioId());
+
+        if (reservaAreaRepository.findByDataAndHoraInicioAndHoraFim(reservaMoradorDTO.getData(),
+                horario.getHoraInicio(), horario.getHoraFim()).isPresent()) {
+            throw new IllegalArgumentException(
+                    "Já existe uma reserva cadastrada para a área com o mesmo horário de início ou fim");
+        }
+
+        // var morador = moradorService.findById(reservaMoradorDTO.getMoradorId());
+
+        var morador = moradorService.findMoradorByEmail("nome@example.com");
+
+        // if (area.getReservas().stream().anyMatch(r ->
+        // r.getHoraInicio().equals(horario.getHoraInicio())
+        // || r.getHoraFim().equals(horario.getHoraFim()))) {
+        // throw new IllegalArgumentException("Já existe uma reserva cadastrada para a
+        // área com id: "
+        // + area.getId() + " com o mesmo horário de início ou fim");
+        // }
+
+        Reserva reserva = new Reserva();
+        ReservaMorador reservaMorador = new ReservaMorador();
+
+        reserva.setData(reservaMoradorDTO.getData());
+        reserva.setHoraInicio(horario.getHoraInicio());
+        reserva.setHoraFim(horario.getHoraFim());
+        reserva.setStatusReserva(StatusReserva.PENDENTE);
+
+        reservaMorador.setMorador(morador);
+        reservaMorador.setReserva(reserva);
+
+        reservaMoradorService.save(reservaMorador);
+
+        // areaRepository.save(area);
+
+        return ResponseEntity.ok().build();
+
     }
 
 }
