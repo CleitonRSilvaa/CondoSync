19b590e37879ef55fbaec8aa382c786bee520314
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-15 21:05:54 -0300

    feat: Add ReservaAreaRepository interface
    A new interface `ReservaAreaRepository` has been added in the `ReservaAreaRepository.java` file. This interface extends the `JpaRepository` interface and provides methods for managing `Reserva` entities related to areas. This addition enhances the application's data handling capabilities and allows for retrieving and manipulating reservations for areas.

---
+++ Back-end/src/main/java/com/CondoSync/repositores/ReservaAreaRepository.java
@@ -0,0 +1,15 @@
+package com.CondoSync.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.CondoSync.models.Reserva;
+import java.util.List;
+import java.util.Optional;
+import java.util.Date;
+import java.time.LocalTime;
+
+public interface ReservaAreaRepository extends JpaRepository<Reserva, Integer> {
+
+    Optional<Reserva> findByDataAndHoraInicioAndHoraFim(Date data, LocalTime horaInicio, LocalTime horaFim);
+
+}
