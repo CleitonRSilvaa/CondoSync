682ef83110e28da9cc99262ad4f8cf877f6d6d93
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-15 21:04:21 -0300

    feat: Add ReservaMorador model for managing reservations
    A new model class `ReservaMorador` has been added in the `ReservaMorador.java` file. This model represents the association between a `Morador` and a `Reserva` for managing reservations. It has a many-to-one relationship with both `Morador` and `Reserva` entities. This addition enhances the application's data handling capabilities and allows for managing reservations for residents.

---
+++ Back-end/src/main/java/com/CondoSync/models/ReservaMorador.java
@@ -0,0 +1,44 @@
+package com.CondoSync.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "reservas_moradores")
+public class ReservaMorador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reserva_morador_id")
+    private Integer id;
+
+    @NotNull(message = "O morador é obrigatorio")
+    @ManyToOne
+    @JoinColumn(name = "morador_id", nullable = false)
+    private Morador morador;
+
+    @NotNull(message = "A reserva é obrigatorio")
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "reserva_id", nullable = false)
+    private Reserva reserva;
+
+}
