ceb47c2c8950e8dba5ecbc17fe8f313cfb425c67
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-17 21:40:03 -0300

    refactor: Update HorarioRepository to add query for finding available horarios
    The code changes in HorarioRepository.java add a new query method, findAvailableHorariosByAreaIdAndDate, to retrieve available horarios based on the areaId and date parameters. This refactor enhances the functionality of the repository by providing a convenient way to fetch horarios that are not already reserved for a specific area and date combination.

--- Back-end/src/main/java/com/CondoSync/repositores/HorarioRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/HorarioRepository.java
@@ -1,17 +1,24 @@
 package com.CondoSync.repositores;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.CondoSync.models.Horario;
 
 public interface HorarioRepository extends JpaRepository<Horario, Integer> {
 
-    List<Horario> findAllByAreaId(UUID id);
+    List<Horario> findAllByAreaIdOrderByHoraInicio(UUID id);
 
     Optional<Horario> findByAreaId(UUID id);
+
+    @Query("SELECT h FROM Horario h WHERE h.area.id = :areaId AND NOT EXISTS " +
+            "(SELECT r FROM Reserva r WHERE r.data = :data AND r.horaInicio = h.horaInicio AND r.horaFim = h.horaFim)")
+    List<Horario> findAvailableHorariosByAreaIdAndDate(@Param("areaId") UUID areaId, @Param("data") Date data);
 
 }
