883fa2112f6fe34c3ae778b1066e8566a6c414d9
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-17 21:40:35 -0300

    refactor: Update ReservaMoradorService to add methods for listing all reservas and filtering by user
    The code changes in ReservaMoradorService.java add new methods for listing all reservas and filtering them by user. The listAll() method retrieves all reservas, while the listAll(User user) method filters the reservas based on the provided user. This refactor enhances the functionality of the service by providing convenient ways to fetch reservas, improving the user experience and flexibility of the codebase.

--- Back-end/src/main/java/com/CondoSync/services/ReservaMoradorService.java
+++ Back-end/src/main/java/com/CondoSync/services/ReservaMoradorService.java
@@ -1,9 +1,18 @@
 package com.CondoSync.services;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.CondoSync.models.Reserva;
 import com.CondoSync.models.ReservaMorador;
+import com.CondoSync.models.StatusReserva;
+import com.CondoSync.models.User;
+import com.CondoSync.models.DTOs.ReservaMoradorDTO;
+import com.CondoSync.repositores.ReservaAreaRepository;
 import com.CondoSync.repositores.ReservaMoradorRepository;
 
 import jakarta.validation.Valid;
@@ -14,9 +23,70 @@ public class ReservaMoradorService {
     @Autowired
     private ReservaMoradorRepository reservaMoradorRepository;
 
+    @Autowired
+    private ReservaAreaRepository reservaAreaRepository;
+
+    @Autowired
+    private AreaService areaService;
+
+    @Autowired
+    private HorarioService horarioService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private MoradorService moradorService;
+
     public ReservaMorador save(@Valid ReservaMorador reservaMorador) {
 
         return reservaMoradorRepository.save(reservaMorador);
+    }
+
+    public List<Reserva> listAll() {
+
+        var reservasMoradores = reservaMoradorRepository.findAll();
+
+        return reservasMoradores.stream().map(ReservaMorador::getReserva).toList();
+
+    }
+
+    public List<Reserva> listAll(User user) {
+
+        var morador = moradorService.findMoradorByEmail(user.getUsername());
+
+        var reservasMoradores = reservaMoradorRepository.findByMorador_Id(morador.getId());
+        return reservasMoradores.stream().map(ReservaMorador::getReserva).toList();
+
+    }
+
+    public ResponseEntity<?> reservarArea(ReservaMoradorDTO reservaMoradorDTO) {
+
+        var horario = horarioService.findById(reservaMoradorDTO.getHorarioId());
+
+        if (reservaAreaRepository.findByDataAndHoraInicioAndHoraFim(reservaMoradorDTO.getData(),
+                horario.getHoraInicio(), horario.getHoraFim()).isPresent()) {
+            throw new IllegalArgumentException(
+                    "Já existe uma reserva cadastrada para a área com o mesmo horário!");
+        }
+
+        var morador = moradorService.findMoradorByEmail("nome@example.com");
+
+        Reserva reserva = new Reserva();
+        ReservaMorador reservaMorador = new ReservaMorador();
+
+        reserva.setData(reservaMoradorDTO.getData());
+        reserva.setHoraInicio(horario.getHoraInicio());
+        reserva.setHoraFim(horario.getHoraFim());
+        reserva.setStatusReserva(StatusReserva.PENDENTE);
+
+        reservaMorador.setMorador(morador);
+        reservaMorador.setReserva(reserva);
+
+        reservaMoradorRepository.save(reservaMorador);
+
+        return ResponseEntity.ok().build();
+
     }
 
 }
