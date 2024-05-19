79e4c10f9429382651ca8784f13ddcd4d3d718d0
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-18 22:02:45 -0300

    refactor: Add Ocorrencia model
    The code changes in Ocorrencia.java add a new model, Ocorrencia, which represents an occurrence. This refactor enhances the codebase by providing a structured way to manage and track occurrences.

--- Back-end/src/main/java/com/CondoSync/models/Ocorrencia.java
+++ Back-end/src/main/java/com/CondoSync/models/Ocorrencia.java
@@ -5,8 +5,33 @@ import java.time.Instant;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ocorrencias")
 public class Ocorrencia {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ocorrencia_id")
     private Integer id;
 
     private String title;
