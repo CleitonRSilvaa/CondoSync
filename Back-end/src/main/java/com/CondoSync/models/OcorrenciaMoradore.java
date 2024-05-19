781974e37b902c08d0ee9a846baa5554cca04e21
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-18 22:02:37 -0300

    refactor: Add OcorrenciaMoradore model
    The code changes add a new model, OcorrenciaMoradore, which represents the association between a Morador and an Ocorrencia. This refactor enhances the codebase by providing a structured way to manage and track occurrences related to residents.

---
+++ Back-end/src/main/java/com/CondoSync/models/OcorrenciaMoradore.java
@@ -0,0 +1,41 @@
+package com.CondoSync.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ocorrencias_moradores")
+public class OcorrenciaMoradore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ocorrencia_morador_id")
+    private Integer id;
+
+    @NotNull(message = "O morador é obrigatorio")
+    @ManyToOne
+    @JoinColumn(name = "morador_id", nullable = false)
+    private Morador morador;
+
+    @NotNull(message = "A ocorrências é obrigatorio")
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ocorrencia_id", nullable = false)
+    private Ocorrencia ocorrencia;
+
+}
