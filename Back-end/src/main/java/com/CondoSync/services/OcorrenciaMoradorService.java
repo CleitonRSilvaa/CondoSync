b774b6337ef30c9b8e2912932433fa1bd1d5f1a9
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-19 21:16:23 -0300

    refactor: Add OcorrenciaMoradorService
    The code changes add a new service, OcorrenciaMoradorService, which provides methods for retrieving, creating, updating, and canceling occurrences related to residents. This refactor enhances the codebase by encapsulating the business logic for managing occurrences and improving the separation of concerns.

---
+++ Back-end/src/main/java/com/CondoSync/services/OcorrenciaMoradorService.java
@@ -0,0 +1,124 @@
+package com.CondoSync.services;
+
+import java.util.stream.Collectors;
+
+import java.util.List;
+
+import com.CondoSync.repositores.OcorrenciaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.CondoSync.models.Ocorrencia;
+import com.CondoSync.models.OcorrenciaMorador;
+import com.CondoSync.models.StatusOcorrencia;
+import com.CondoSync.models.DTOs.OcorenciaDTO;
+import com.CondoSync.repositores.OcorrenciaMoradorRepository;
+
+@Service
+public class OcorrenciaMoradorService {
+
+    @Autowired
+    private OcorrenciaMoradorRepository ocorrenciaMoradorRepository;
+
+    @Autowired
+    private OcorrenciaRepository ocorrenciaRepository;
+
+    @Autowired
+    private MoradorService moradorService;
+
+    public List<OcorenciaDTO> getOcorrenciaMorador(String moradorUserName) {
+
+        var morador = moradorService.findMoradorByEmail(moradorUserName);
+
+        var ocorrenciaMorador = ocorrenciaMoradorRepository.findByMorador_Id(morador.getId());
+
+        var resultList = ocorrenciaMorador.stream()
+                .map(OcorrenciaMorador::getOcorrencia)
+                .map(OcorenciaDTO::new)
+                .sorted(
+                        (o1, o2) -> {
+                            if (o1.getStatus().equals(StatusOcorrencia.ABERTO.getStatus())
+                                    && !o2.getStatus().equals(StatusOcorrencia.ABERTO.getStatus())) {
+                                return -1;
+                            } else if (!o1.getStatus().equals(StatusOcorrencia.ABERTO.getStatus())
+                                    && o2.getStatus().equals(StatusOcorrencia.ABERTO.getStatus())) {
+                                return 1;
+                            } else {
+                                return o1.getCreation().compareTo(o2.getCreation());
+                            }
+                        })
+                .collect(Collectors.toList());
+
+        return resultList;
+    }
+
+    public ResponseEntity<?> getOcorrenciaMoradorById(Integer id) {
+        return ResponseEntity.ok().body(ocorrenciaMoradorRepository.findById(id));
+    }
+
+    public ResponseEntity<?> createOcorrenciaMorador(Ocorrencia ocorrencia, String moradorUserName) {
+
+        var morador = moradorService.findMoradorByEmail(moradorUserName);
+
+        var ocorrenciaMorador = new OcorrenciaMorador();
+
+        ocorrencia.setStatus(StatusOcorrencia.ABERTO);
+
+        ocorrenciaMorador.setOcorrencia(ocorrencia);
+        ocorrenciaMorador.setMorador(morador);
+
+        return ResponseEntity.ok().body(ocorrenciaMoradorRepository.save(ocorrenciaMorador));
+    }
+
+    public ResponseEntity<?> createOcorrenciaMorador(OcorenciaDTO ocorenciaDTO, String moradorUserName) {
+
+        var morador = moradorService.findMoradorByEmail(moradorUserName);
+
+        var ocorrenciaMorador = new OcorrenciaMorador();
+
+        var ocorrencia = new Ocorrencia();
+
+        ocorrencia.setTitle(ocorenciaDTO.getTitle());
+        ocorrencia.setDescription(ocorenciaDTO.getDescription());
+        ocorrencia.setStatus(StatusOcorrencia.ABERTO);
+
+        ocorrenciaMorador.setOcorrencia(ocorrencia);
+        ocorrenciaMorador.setMorador(morador);
+
+        System.out.println(morador);
+
+        ocorrenciaMoradorRepository.save(ocorrenciaMorador);
+
+        return ResponseEntity.ok().body("");
+    }
+
+    public ResponseEntity<?> cancelOcorrenciaMorador(Integer id, String moradorUserName) {
+        OcorrenciaMorador ocorrenciaMorador = ocorrenciaMoradorRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Ocorrencia não encontrada!"));
+
+        if (ocorrenciaMorador.getOcorrencia().getStatus().equals(StatusOcorrencia.CANCELADA)) {
+            throw new IllegalArgumentException("Ocorrencia já cancelada");
+        }
+
+        if (ocorrenciaMorador.getOcorrencia().getStatus().equals(StatusOcorrencia.RESOLVIDA)) {
+            throw new IllegalArgumentException("Ocorrencia já resolvida");
+        }
+
+        ocorrenciaMorador.getOcorrencia().setStatus(StatusOcorrencia.CANCELADA);
+        ocorrenciaMoradorRepository.save(ocorrenciaMorador);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
+
+    public ResponseEntity<?> updateOcorrenciaMorador(Integer id, OcorrenciaMorador ocorrenciaMorador) {
+        OcorrenciaMorador ocorrenciaMorador2 = ocorrenciaMoradorRepository.findById(id).get();
+        ocorrenciaMorador2.setMorador(ocorrenciaMorador.getMorador());
+        ocorrenciaMorador2.setOcorrencia(ocorrenciaMorador.getOcorrencia());
+        return ResponseEntity.ok().body(ocorrenciaMoradorRepository.save(ocorrenciaMorador2));
+    }
+
+}
