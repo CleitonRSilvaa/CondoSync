2f031a195ecbd5ecfc982a2e9164c2cf48edc902
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-20 21:21:39 -0300

    refactor: Update UserService to use @Transactional for createUser method
    The code changes in UserService.java add the @Transactional annotation to the createUser method. This refactor enhances the codebase by ensuring that the user creation process is executed within a transaction, providing data consistency and rollback capabilities in case of failures.

--- Back-end/src/main/java/com/CondoSync/services/UserService.java
+++ Back-end/src/main/java/com/CondoSync/services/UserService.java
@@ -10,11 +10,16 @@ import org.springframework.stereotype.Service;
 import com.CondoSync.models.User;
 import com.CondoSync.repositores.UsersRepository;
 
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserService implements UserDetailsService {
 
     @Autowired
@@ -43,10 +48,29 @@ public class UserService implements UserDetailsService {
         return usersRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public User createUser(User user) {
+        try {
+            var userSave = usersRepository.save(user);
+            log.info("User created successfully");
+            return userSave;
+        } catch (PersistenceException pe) {
+            log.error("Failed to create user: {}", pe.getMessage());
+            throw pe;
+        }
 
-        return usersRepository.save(user);
     }
+
+    // public void createUser(User user) {
+    // log.info("Creating user: {}", user.getUsername());
+    // try {
+    // usersRepository.persist(user);
+    // log.info("User created successfully");
+    // } catch (PersistenceException pe) {
+    // log.error("Failed to create user: {}", pe.getMessage());
+    // throw pe;
+    // }
+    // }
 
     public User updateUser(User user) {
         return usersRepository.save(user);
