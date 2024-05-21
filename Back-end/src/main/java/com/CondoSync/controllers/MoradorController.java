f8d4e439f8780e3ad94a7d42cf58926167b9aee5
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-21 20:50:19 -0300

    refactor: Update MoradorController to include find, update, and findById endpoints

--- Back-end/src/main/java/com/CondoSync/controllers/MoradorController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/MoradorController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.CondoSync.models.User;
 import com.CondoSync.models.DTOs.MoradorDTO;
+import com.CondoSync.models.DTOs.ResponseDTO;
 import com.CondoSync.services.MoradorService;
 import com.CondoSync.services.UserService;
 
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +22,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -66,6 +71,53 @@ public class MoradorController {
     @GetMapping("/list")
     public ResponseEntity<?> listAll() {
         return new ResponseEntity<>(moradorService.listAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @GetMapping("/find")
+    public ResponseEntity<?> findByEmail(@RequestParam String email) {
+        return new ResponseEntity<>(moradorService.findMoradorByEmail(email), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PutMapping("/update/{moradorId}")
+    public ResponseEntity<?> update(@RequestBody @Valid MoradorDTO moradorDTO, @PathVariable UUID moradorId) {
+
+        System.out.println("moradorId: " + moradorId);
+        System.out.println("moradorDTO.getId(): " + moradorDTO.getId());
+
+        if (!moradorDTO.getId().toString().equals(moradorId.toString())) {
+
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setMessage("Id do morador não corresponde ao id do corpo da requisição");
+            responseDTO.setError("Id do morador não corresponde ao id do corpo da requisição");
+            responseDTO.setStatus(400);
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+
+        }
+
+        return new ResponseEntity<>(moradorService.update(moradorId, moradorDTO), HttpStatus.OK);
+    }
+
+    // @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    // @PostMapping("/delete/{moradorId}")
+    // public ResponseEntity<?> delete(@PathVariable UUID moradorId) {
+    // return new ResponseEntity<>(moradorService.delete(moradorId), HttpStatus.OK);
+    // }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PutMapping("/change-status/{moradorId}")
+    public ResponseEntity<?> changeStatus(@PathVariable UUID moradorId) {
+        return new ResponseEntity<>(moradorService.changeStatus(moradorId), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @GetMapping("/find/{moradorId}")
+    public ResponseEntity<?> findById(@PathVariable UUID moradorId) {
+
+        var morador = moradorService.findById(moradorId);
+
+        return new ResponseEntity<>(new MoradorDTO(morador), HttpStatus.OK);
     }
 
 }
