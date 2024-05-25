0e12a9c283dac32a267d278e9589b2948f99d4fc
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-24 22:39:18 -0300

    refactor: Update ReservaRepository to use LocalDate instead of Date for findByDataAndHoraInicioAndHoraFim

--- Back-end/src/main/java/com/CondoSync/repositores/ReservaRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/ReservaRepository.java
@@ -3,13 +3,16 @@ package com.CondoSync.repositores;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.CondoSync.models.Reserva;
+import com.CondoSync.models.StatusReserva;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.Date;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-    Optional<Reserva> findByDataAndHoraInicioAndHoraFim(Date data, LocalTime horaInicio, LocalTime horaFim);
+    Optional<Reserva> findByDataAndHoraInicioAndHoraFim(LocalDate data, LocalTime horaInicio, LocalTime horaFim);
 
+    List<Reserva> findAllByStatusReserva(StatusReserva statusReserva);
 }
