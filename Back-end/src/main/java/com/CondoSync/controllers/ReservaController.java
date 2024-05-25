553ea28de9a32e25f29491eedb3ac9c7943cc49d
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-24 22:41:39 -0300

    refactor: Update ReservaController to use getAllReservas instead of listAll

--- Back-end/src/main/java/com/CondoSync/controllers/ReservaController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/ReservaController.java
@@ -1,14 +1,11 @@
 package com.CondoSync.controllers;
 
-import javax.swing.Spring;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
