bcf9b03025d744caf8111ff922590a73be4f6c63
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-24 22:39:29 -0300

    refactor: Update OcorrenciaMoradorRepository to use List instead of java.util.List
    The code changes in OcorrenciaMoradorRepository.java update the import statement for the List class to use the correct List interface instead of the incorrect java.util.List class. This refactor ensures that the correct List interface is used, avoiding potential compilation errors and improving code clarity.

--- Back-end/src/main/java/com/CondoSync/repositores/OcorrenciaMoradorRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/OcorrenciaMoradorRepository.java
@@ -1,14 +1,14 @@
 package com.CondoSync.repositores;
 
+import java.util.List;
 import java.util.UUID;
 
-import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.CondoSync.models.OcorrenciaMorador;
 
 public interface OcorrenciaMoradorRepository extends JpaRepository<OcorrenciaMorador, Integer> {
 
-  java.util.List<OcorrenciaMorador> findByMorador_Id(UUID moradorId);
+  List<OcorrenciaMorador> findByMorador_Id(UUID moradorId);
 
 }
