cfa9986575aabbad6dff6ae1a7a9afdf3ec5bdfa
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-26 01:18:11 -0300

    refactor: Update image links, add token decoding and redirect logic, and improve loading overlay styles and z-index values

--- Back-end/src/main/resources/static/js/script-manage-reservas.js
+++ Back-end/src/main/resources/static/js/script-manage-reservas.js
@@ -5,7 +5,8 @@ import * as token from "/js/auth.js";
 const baseUrl = "http://localhost:8010";
 
 document.addEventListener("DOMContentLoaded", () => {
-  getOcorrencias();
+  token.validateSecurity();
+  geReservas();
 });
 
 function showLoading() {
@@ -48,12 +49,12 @@ function showToast(titulo, message, clss = "bg-primary", time = 5000) {
   toast.show();
 }
 
-async function getOcorrencias() {
+async function geReservas() {
   token.validateSecurity();
   showLoading();
 
   try {
-    const response = await fetch(`${baseUrl}/api/v1/ocorrencia/listAll`, {
+    const response = await fetch(`${baseUrl}/api/v1/reserva/list`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -65,9 +66,9 @@ async function getOcorrencias() {
 
     if (response.ok) {
       if (data.length === 0) {
-        const container = document.getElementById("ocorrencias-container");
+        const container = document.getElementById("reservas-container");
         const p = document.createElement("p");
-        p.textContent = "Nenhuma ocorrência encontrada.";
+        p.textContent = "Nenhuma reserva encontrada.";
         p.classList.add("text-center", "text-uppercase", "fw-bold");
         container.innerHTML = "";
         container.appendChild(p);
@@ -90,9 +91,9 @@ async function getOcorrencias() {
       showToast("Atenção", data.message, "bg-warning", 7000);
       return;
     }
-    showToast("Atenção", "Erro ao buscar as ocorrências!", "bg-warning", 7000);
+    showToast("Atenção", "Erro ao buscar as reservas!", "bg-warning", 7000);
   } catch (error) {
-    showToast("Erro", "Erro ao buscar as ocorrências!", "bg-danger", 5000);
+    showToast("Erro", "Erro ao buscar as reservas!", "bg-danger", 5000);
   } finally {
     hideLoading();
   }
@@ -101,6 +102,14 @@ const StatusOcorrencia = Object.freeze({
   ABERTO: "Em aberto",
   EM_ANDAMENTO: "Em andamento",
   RESOLVIDA: "Resolvida",
+  CANCELADA: "Cancelada",
+});
+
+const StatusReserva = Object.freeze({
+  PENDENTE: "Pendente",
+  APROVADA: "Aprovada",
+  FINALIZADA: "Finalizada",
+  REJEITADA: "Rejeitada",
   CANCELADA: "Cancelada",
 });
 
@@ -113,18 +122,19 @@ function inverterObjeto(obj) {
 }
 
 function buildTable(data) {
-  const container = document.getElementById("ocorrencias-container");
+  const container = document.getElementById("reservas-container");
   const table = document.createElement("table");
   table.className = "table table-striped table-hover";
   table.innerHTML = `
     <thead class="table-dark">
       <tr>
         <th scope="col">ID</th>
-        <th scope="col">Morador</th>
-        <th scope="col">Titulo</th>
+        <th scope="col">Espaço</th>
         <th scope="col">Data</th>
+        <th scope="col">Horário</th>
+        <th scope="col">Morador</th>
         <th scope="col">Status</th>
-        <th scope="col">Ações</th>
+        <th scope="col" colspan="1" class="text-center">Ações</th>
       </tr>
     </thead>
     <tbody>
@@ -134,44 +144,35 @@ function buildTable(data) {
   const tbody = table.querySelector("tbody");
 
   let count = 1;
-  data.forEach((ocorrencia) => {
+  data.forEach((reserva) => {
     const row = tbody.insertRow();
+    row.insertCell().textContent = reserva.id;
+    row.insertCell().textContent = reserva.area;
+    row.insertCell().textContent = reserva.data;
+    row.insertCell().textContent = reserva.horario;
+    row.insertCell().textContent = reserva.morador;
+    const cell = row.insertCell();
+    cell.textContent = reserva.status;
 
-    row.insertCell().textContent = ocorrencia.id;
-    row.insertCell().textContent = ocorrencia.title;
-    row.insertCell().textContent = ocorrencia.morador.nomeCompleto;
-    row.insertCell().textContent = ocorrencia.creation;
-    row.insertCell().textContent = ocorrencia.status;
+    const collor = {
+      PENDENTE: "text-warning",
+      APROVADA: "text-success",
+      FINALIZADA: "text-primary",
+      REJEITADA: "text-danger",
+      CANCELADA: "text-secondary",
+    };
+
+    cell.classList.add(collor[inverterObjeto(StatusReserva)[reserva.status]]);
 
     const actionCell = row.insertCell();
     actionCell.className = "text-center";
-
-    const isResolvida =
-      ocorrencia.status === StatusOcorrencia.RESOLVIDA.toString();
-
-    if (
-      ocorrencia.status === StatusOcorrencia.CANCELADA.toString() ||
-      ocorrencia.status === StatusOcorrencia.RESOLVIDA.toString()
-    ) {
-      const button = document.createElement("button");
-      button.classList.add(
-        "btn",
-        isResolvida ? "btn-success" : "btn-secondary",
-        "btn-sm",
-        "me-2"
-      );
-      button.textContent = "Visualizar";
-      button.dataset.bsToggle = "modal";
-      button.dataset.bsTarget = "#exampleModal";
-      button.onclick = () => changeModalEdit(ocorrencia);
-      actionCell.appendChild(button);
-    } else {
+    if (reserva.status === StatusReserva.PENDENTE.toString()) {
       const button = document.createElement("button");
       button.classList.add("btn", "btn-warning", "btn-sm", "me-2");
-      button.textContent = "Resolver";
+      button.textContent = "Validar";
       button.dataset.bsToggle = "modal";
       button.dataset.bsTarget = "#exampleModal";
-      button.onclick = () => changeModalEdit(ocorrencia);
+      button.onclick = () => changeModalEdit(reserva);
       actionCell.appendChild(button);
     }
   });
@@ -183,34 +184,25 @@ function buildTable(data) {
   container.appendChild(div);
 }
 
-function changeModalEdit(ocorrencia) {
+function changeModalEdit(reserva) {
+  console.log(reserva);
   const modal = document.getElementById("exampleModal");
   const modalTitle = modal.querySelector(".modal-title");
   const modalFooter = modal.querySelector(".modal-footer");
-  modalTitle.textContent = `Ocorrência ${
-    ocorrencia.id + " - " + ocorrencia.creation
-  }`;
+  modalTitle.textContent = `Reserva ${reserva.id + " - " + reserva.data}`;
 
-  const moradorInfo = document.getElementById("moradorInfo");
-  moradorInfo.value = `${ocorrencia.morador.nomeCompleto} - AP: ${
-    ocorrencia.morador.apartamento
-  } - Celular: ${
-    ocorrencia.morador.celular ? ocorrencia.morador.celular : "Não informado"
-  }`;
+  const statusReserva = document.getElementById("statusReserva");
+  // statusreserva.value = reserva.status;
+  const respostareserva = document.getElementById("respostaReserva");
+  respostareserva.value = reserva.resolution ? reserva.resolution : "";
 
-  const tituloOcorrencia = document.getElementById("tituloOcorrencia");
-  tituloOcorrencia.value = ocorrencia.title;
-  const descricaoOcorrencia = document.getElementById("descricaoOcorrencia");
-  descricaoOcorrencia.value = ocorrencia.description;
-  const statusOcorrencia = document.getElementById("statusOcorrencia");
-  // statusOcorrencia.value = ocorrencia.status;
-  const respostaOcorrencia = document.getElementById("respostaOcorrencia");
-  respostaOcorrencia.value = ocorrencia.resolution ? ocorrencia.resolution : "";
-
-  const StatusOcorrenciaInvertido = inverterObjeto(StatusOcorrencia);
-  statusOcorrencia.innerHTML = "";
-  for (const [chave, valor] of Object.entries(StatusOcorrenciaInvertido)) {
-    if (ocorrencia.status !== "Em aberto" && valor === "ABERTO") {
+  const StatusReservaInvertido = inverterObjeto(StatusReserva);
+  statusReserva.innerHTML = "";
+  for (const [chave, valor] of Object.entries(StatusReservaInvertido)) {
+    if (
+      (reserva.status !== "Pendente" && valor === "PENDENTE") ||
+      valor === "FINALIZADA"
+    ) {
       continue;
     }
 
@@ -218,11 +210,11 @@ function changeModalEdit(ocorrencia) {
     option.value = valor;
     option.textContent = chave;
 
-    if (ocorrencia.status === chave) {
+    if (reserva.status === chave) {
       option.selected = true;
     }
 
-    statusOcorrencia.appendChild(option);
+    statusReserva.appendChild(option);
   }
 
   modalFooter.innerHTML = "";
@@ -232,29 +224,35 @@ function changeModalEdit(ocorrencia) {
   buttonFechar.setAttribute("data-bs-dismiss", "modal");
   modalFooter.appendChild(buttonFechar);
 
-  if (
-    ocorrencia.status === "Em aberto" ||
-    ocorrencia.status === "Em andamento"
-  ) {
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-success");
-    button.textContent = "Resolver";
-    button.onclick = () => resolverOcorrencia(ocorrencia);
-    modalFooter.appendChild(button);
-  }
-  statusOcorrencia.disabled = false;
-  respostaOcorrencia.disabled = false;
-  if (ocorrencia.status === "Resolvida" || ocorrencia.status === "Cancelada") {
-    const bntMudaStatus = document.createElement("button");
-    bntMudaStatus.classList.add("btn", "btn-warning");
-    bntMudaStatus.textContent = "Mudar Status";
-    bntMudaStatus.onclick = () => resolverOcorrencia(ocorrencia);
-    modalFooter.appendChild(bntMudaStatus);
-    respostaOcorrencia.disabled = true;
-  }
+  const buttonSalvar = document.createElement("button");
+  buttonSalvar.classList.add("btn", "btn-success");
+  buttonSalvar.textContent = "Salvar";
+  buttonSalvar.onclick = () => resolverReserva(reserva);
+  modalFooter.appendChild(buttonSalvar);
+
+  // if (
+  //   ocorrencia.status === "Em aberto" ||
+  //   ocorrencia.status === "Em andamento"
+  // ) {
+  //   const button = document.createElement("button");
+  //   button.classList.add("btn", "btn-success");
+  //   button.textContent = "Resolver";
+  //   button.onclick = () => resolverOcorrencia(ocorrencia);
+  //   modalFooter.appendChild(button);
+  // }
+  // statusOcorrencia.disabled = false;
+  // respostaOcorrencia.disabled = false;
+  // if (ocorrencia.status === "Resolvida" || ocorrencia.status === "Cancelada") {
+  //   const bntMudaStatus = document.createElement("button");
+  //   bntMudaStatus.classList.add("btn", "btn-warning");
+  //   bntMudaStatus.textContent = "Mudar Status";
+  //   bntMudaStatus.onclick = () => resolverOcorrencia(ocorrencia);
+  //   modalFooter.appendChild(bntMudaStatus);
+  //   respostaOcorrencia.disabled = true;
+  // }
 }
 
-async function resolverOcorrencia(ocorrencia) {
+async function resolverReserva2(ocorrencia) {
   const statusOcorrencia = document.getElementById("statusOcorrencia").value;
   const respostaOcorrencia =
     document.getElementById("respostaOcorrencia").value;
@@ -326,3 +324,85 @@ function closeModal() {
     bootstrap.Modal.getInstance(modal) || new bootstrap.Modal(modal);
   modalInstance.hide();
 }
+
+async function resolverReserva(reserva) {
+  const statusReserva = document.getElementById("statusReserva").value;
+  const respostaReserva = document.getElementById("respostaReserva").value;
+
+  const resolverReservaData = {
+    id: reserva.id,
+    status: statusReserva.toString(),
+  };
+
+  console.log(resolverReservaData);
+
+  showLoading();
+
+  console.log("json", JSON.stringify(resolverReservaData));
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/api/v1/reserva/update/` + reserva.id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token.getToken(),
+        },
+        body: JSON.stringify(resolverReservaData),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      closeModal();
+      showToast(
+        "Sucesso",
+        "Reserva atualizada com sucesso!",
+        "bg-success",
+        5000
+      );
+      // await geReservas();
+      return;
+    }
+    if (response.status === 403 || response.status === 401) {
+      showToast(
+        "Atenção",
+        "Você não tem permissão para acessar essa página!",
+        "bg-warning",
+        7000
+      );
+      return;
+    }
+    if (response.status === 400 || response.status === 404) {
+      showToast("Atenção", data.message, "bg-warning", 7000);
+      return;
+    }
+    showToast("Atenção", "Erro ao resolver a reserva!", "bg-warning", 7000);
+  } catch (error) {
+    showToast("Erro", "Erro ao resolver a reserva!", "bg-danger", 5000);
+  } finally {
+    hideLoading();
+  }
+}
+
+document.getElementById("statusReserva").addEventListener("change", (event) => {
+  console;
+  const respostareserva = document.getElementById("respostaReserva");
+  const respostareservadiv = document.getElementById("respostaReserva-div");
+
+  if (
+    event.target.value === "REJEITADA" ||
+    event.target.value === "CANCELADA"
+  ) {
+    respostareserva.disabled = false;
+    //respostareservadiv.style.display = "block";
+    respostareservadiv.removeAttribute("hidden");
+  } else {
+    respostareserva.value = "";
+    respostareserva.disabled = true;
+    respostareservadiv.setAttribute("hidden", true);
+    //respostareservadiv.style.display = "none";
+  }
+});
