69a3425c034978c596b8e9f2e48d49d8346eb863
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 17:04:08 -0300

    refactor: Add query method to find Reserva by data, horaInicio, horaFim, and not statusReserva

--- Back-end/src/main/java/com/CondoSync/repositores/ReservaRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/ReservaRepository.java
@@ -1,6 +1,8 @@
 package com.CondoSync.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.CondoSync.models.Reserva;
 import com.CondoSync.models.StatusReserva;
@@ -13,6 +15,13 @@ import java.time.LocalTime;
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
     Optional<Reserva> findByDataAndHoraInicioAndHoraFim(LocalDate data, LocalTime horaInicio, LocalTime horaFim);
+
+    @Query("SELECT r FROM Reserva r WHERE r.data = :data AND r.horaInicio = :horaInicio AND r.horaFim = :horaFim AND r.statusReserva != :statusReserva")
+    Optional<Reserva> findByDataAndHoraInicioAndHoraFimAndNotStatus(
+            @Param("data") LocalDate data,
+            @Param("horaInicio") LocalTime horaInicio,
+            @Param("horaFim") LocalTime horaFim,
+            @Param("statusReserva") StatusReserva statusReserva);
 
     List<Reserva> findAllByStatusReserva(StatusReserva statusReserva);
 }
