de67599788274ec71d2294f04729285c4a57555e
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-26 21:04:04 -0300

    feat: Add UpdateStatusReservaDTO and update ReservaController for updating reservation status

--- Back-end/src/main/java/com/CondoSync/controllers/ReservaController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/ReservaController.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.CondoSync.models.User;
 
 import com.CondoSync.models.DTOs.NewReservaDTO;
+import com.CondoSync.models.DTOs.ReservaDTO;
+import com.CondoSync.models.DTOs.UpdateStatusReservaDTO;
 import com.CondoSync.services.ReservaMoradorService;
 import com.CondoSync.services.UserService;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -35,7 +45,7 @@ public class ReservaController {
     @PreAuthorize("hasAuthority('SCOPE_ADMIN') or hasAuthority('SCOPE_MORADOR')")
     @PostMapping("/register")
     public ResponseEntity<?> register(
-            JwtAuthenticationToken jwtAuthenticationToken, @RequestBody NewReservaDTO reservaMoradorDto) {
+            JwtAuthenticationToken jwtAuthenticationToken, @RequestBody @Valid NewReservaDTO reservaMoradorDto) {
         User user = userService.findByUserName(jwtAuthenticationToken.getToken().getSubject()).orElseThrow(
                 () -> new UsernameNotFoundException(
                         "Usuário não encontrado: " + jwtAuthenticationToken.getToken().getSubject()));
@@ -88,6 +98,28 @@ public class ReservaController {
         // }
 
         return reservaMoradorService.cancelarReserva(id);
+    }
+
+    // @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PostMapping("/update/{id}")
+    public ResponseEntity<?> updateStatus(@RequestBody String reservaMoradorJson, @PathVariable Integer id,
+            JwtAuthenticationToken jwtAuthenticationToken) {
+
+        System.out.println("reservaMoradorJson: " + reservaMoradorJson);
+        // userService.findByUserName(jwtAuthenticationToken.getToken().getSubject()).orElseThrow(
+        // () -> new UsernameNotFoundException(
+        // "Usuário não encontrado: " +
+        // jwtAuthenticationToken.getToken().getSubject()));
+
+        // if (!user.getAuthorities().toString().toUpperCase().contains("ADMIN")) {
+        // return new ResponseEntity<>("Usuário não autorizado",
+        // HttpStatus.UNAUTHORIZED);
+        // }
+
+        return new ResponseEntity<>(reservaMoradorJson, HttpStatus.OK);
+
+        // return new ResponseEntity<>(reservaMoradorService.updateReserva(id,
+        // reservaMoradorDto), HttpStatus.OK);
     }
 
     @PostMapping("/reservation")
