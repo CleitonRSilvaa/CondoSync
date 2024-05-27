e81ccc443812f2612622ea37a7cae85ad4d74167
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 17:03:33 -0300

    refactor: Update PushManagerController to use new notifications endpoint
    The code changes in PushManagerController.java update the request mapping from "api/v1/push" to "api/v1/notifications". This refactor improves the clarity and consistency of the code by using a more descriptive and accurate endpoint name.

--- Back-end/src/main/java/com/CondoSync/controllers/PushManagerController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/PushManagerController.java
@@ -5,27 +5,35 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.CondoSync.models.User;
 import com.CondoSync.models.DTOs.UserSubscriptionDTO;
+import com.CondoSync.services.UserService;
 import com.CondoSync.services.UserSubscriptionService;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("api/v1/push")
+@RequestMapping("api/v1/notifications")
 @Validated
 public class PushManagerController {
 
     @Autowired
     private UserSubscriptionService userSubscriptionService;
+
+    @Autowired
+    private UserService userService;
 
     @Value("${notify.public.key}")
     private String publicKey;
@@ -45,8 +53,16 @@ public class PushManagerController {
         return ResponseEntity.ok(privateKey);
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN') or hasAuthority('SCOPE_MORADOR')")
     @PostMapping("/subscribe")
-    public ResponseEntity<?> subResponseEntity(@RequestBody @Valid UserSubscriptionDTO userSubscriptionDTO) {
+    public ResponseEntity<?> subResponseEntity(@RequestBody @Valid UserSubscriptionDTO userSubscriptionDTO,
+            JwtAuthenticationToken jwtAuthenticationToken) {
+        User user = userService.findByUserName(jwtAuthenticationToken.getToken().getSubject()).orElseThrow(
+                () -> new UsernameNotFoundException(
+                        "Usuário não encontrado: "
+                                + jwtAuthenticationToken.getToken().getSubject()));
+
+        userSubscriptionDTO.setUser_id(user.getId());
         return userSubscriptionService.saveSubscription(userSubscriptionDTO);
 
     }
