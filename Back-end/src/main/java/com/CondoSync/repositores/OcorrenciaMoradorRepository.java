3fc62119dfbac8482be6ca58a43124a932963016
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 18:49:45 -0300

    refactor: Add findByOcorrencia_Id method to OcorrenciaMoradorRepository

--- Back-end/src/main/java/com/CondoSync/repositores/OcorrenciaMoradorRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/OcorrenciaMoradorRepository.java
@@ -6,9 +6,12 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.CondoSync.models.OcorrenciaMorador;
+import com.CondoSync.models.DTOs.OcorenciaDTO;
 
 public interface OcorrenciaMoradorRepository extends JpaRepository<OcorrenciaMorador, Integer> {
 
   List<OcorrenciaMorador> findByMorador_Id(UUID moradorId);
+
+  OcorrenciaMorador findByOcorrencia_Id(Integer id);
 
 }
