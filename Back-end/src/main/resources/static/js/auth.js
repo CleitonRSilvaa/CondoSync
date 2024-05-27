4f9d9831e2636cb48c820d02a9553e02a911b8ea
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 12:10:46 -0300

    refactor: Update base URL in auth.js
    The code changes in auth.js update the base URL to "http://192.168.0.115:8010". This refactor ensures that the application makes requests to the correct server address, improving the functionality and connectivity of the application.

--- Back-end/src/main/resources/static/js/auth.js
+++ Back-end/src/main/resources/static/js/auth.js
@@ -24,7 +24,10 @@ export function revogeToken() {
 
 export function logout() {
   revogeToken();
-  window.location.href = "/Login/login.html";
+  window.location.href =
+    "/Login/login.html" +
+    "?error=401" +
+    "mgs=Sessão expirada, faça login novamente";
 }
 
 export function getToken() {
@@ -69,18 +72,47 @@ export function getScope() {
   return payload.scope;
 }
 
+const pagesAdmin = [
+  "/admin/index.html",
+  "/admin/gerenciar-reservas.html",
+  "/admin/gerenciar-ocorrencias.html",
+  "/admin/gerenciar-reservas.html",
+  "/admin/gerenciar-moradores.html",
+];
+
+const pagesMorador = [
+  "/morador/index.html",
+  "/morador/perfil/index.html",
+  "/morador/mural/index.html",
+  "/morador/espaco-agendamento/index.html",
+  "/morador/financeiro/index.html",
+  "/morador/ocorrencia/index.html",
+];
+
 export function validateSecurity() {
+  getReferrer();
   if (!isLogged()) {
     window.location.href = "../Login/login.html";
   }
   if (isExpiredToken()) {
-    showToast(
-      "Atenção",
-      "Sua sessão expirou, faça login novamente!",
-      "bg-warning",
-      7000
-    );
     logout();
+  }
+
+  if (!isLoggedAdmin()) {
+    const page = window.location.pathname;
+    console.log("Página atual: ", page);
+    if (pagesAdmin.includes(page)) {
+      window.location.href = "/morador/index.html?error=403";
+      return;
+    }
+  }
+  if (isLoggedAdmin()) {
+    const page = window.location.pathname;
+    console.log("Página atual: ", page);
+    if (pagesMorador.includes(page)) {
+      window.location.href = "/admin/index.html?error=403";
+      return;
+    }
   }
 }
 
@@ -95,3 +127,46 @@ export function validateSecurity() {
 //   }
 //   saveLogin(token);
 // }
+
+// Função para obter a página de referência
+export function getReferrer() {
+  const referrer = document.referrer;
+  console.log("Página de referência: ", referrer);
+  return referrer;
+}
+
+export function isLoggedAdmin() {
+  return isLogged() && getScope().includes("ADMIN");
+}
+
+function showToast(titulo, message, clss = "bg-primary", time = 5000) {
+  const toastContainer = document.getElementById("toastContainer");
+
+  const toastEl = document.createElement("div");
+  toastEl.className = `toast align-items-center text-white border-0 ${clss}`;
+  toastEl.setAttribute("role", "alert");
+  toastEl.setAttribute("aria-live", "assertive");
+  toastEl.setAttribute("aria-atomic", "true");
+  toastEl.dataset.bsDelay = time;
+
+  toastEl.innerHTML = `
+          <div class="toast-header">
+            <span class="text-primary"><i class="fa-solid fa-circle-info fa-lg"></i></span>
+            <strong class="me-auto">${titulo}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+          </div>
+          <div class="toast-body">
+            <span><i class="fa-solid fa-circle-check fa-lg"></i></span>
+            <div class="d-flex flex-grow-1 align-items-center">
+                    ${message}
+                  </div>
+          </div>
+        `;
+
+  toastContainer.appendChild(toastEl);
+
+  toastEl.scrollIntoView({ behavior: "smooth", block: "end" });
+
+  const toast = new bootstrap.Toast(toastEl);
+  toast.show();
+}
