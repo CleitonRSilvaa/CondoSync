d1029b0ea26b0c462f105bf37b1b51504695b0ba
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 12:09:02 -0300

    feat: Add showToast function for displaying toast notifications
    The code changes in script.js introduce a new function, showToast, which is responsible for displaying toast notifications on the page. This feature enhances the user experience by providing a visually appealing and informative way to communicate messages to the user.

---
+++ Back-end/src/main/resources/static/morador/mural/script.js
@@ -0,0 +1,39 @@
+import * as token from "/js/auth.js";
+
+const baseUrl = "http://localhost:8010";
+
+document.addEventListener("DOMContentLoaded", () => {
+  token.validateSecurity();
+});
+
+function showToast(titulo, message, clss = "bg-primary", time = 5000) {
+  const toastContainer = document.getElementById("toastContainer");
+
+  const toastEl = document.createElement("div");
+  toastEl.className = `toast align-items-center text-white border-0 ${clss}`;
+  toastEl.setAttribute("role", "alert");
+  toastEl.setAttribute("aria-live", "assertive");
+  toastEl.setAttribute("aria-atomic", "true");
+  toastEl.dataset.bsDelay = time;
+
+  toastEl.innerHTML = `
+            <div class="toast-header">
+              <span class="text-primary"><i class="fa-solid fa-circle-info fa-lg"></i></span>
+              <strong class="me-auto">${titulo}</strong>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+              <span><i class="fa-solid fa-circle-check fa-lg"></i></span>
+              <div class="d-flex flex-grow-1 align-items-center">
+                      ${message}
+                    </div>
+            </div>
+          `;
+
+  toastContainer.appendChild(toastEl);
+
+  toastEl.scrollIntoView({ behavior: "smooth", block: "end" });
+
+  const toast = new bootstrap.Toast(toastEl);
+  toast.show();
+}
