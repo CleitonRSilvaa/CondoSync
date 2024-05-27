f7bab2914b1b2cf7cd978d8df1c9ff7e74781e8f
Cleiton Silva <cleiton_ribeiro@live.com>
2024-05-27 18:51:42 -0300

    refactor: Update service worker to handle push notifications
    This commit updates the service worker code in sw.js to properly handle push notifications. It adds a getData() function to retrieve the notification data and handle different scenarios. If the user has granted notification permission, the notification is displayed with the appropriate data. If the user has not granted permission, a fallback notification is displayed. Additionally, the notificationclick event listener is updated to open the user's profile page when the notification is clicked. These changes improve the functionality and user experience of push notifications in the application.

--- Back-end/src/main/resources/static/js/sw.js
+++ Back-end/src/main/resources/static/js/sw.js
@@ -1,72 +1,78 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        self.skipWaiting()
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        self.clients.claim()
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('push', function(event) {
-    const getData = async (event) => {
+self.addEventListener("push", function (event) {
+  const getData = async (event) => {
+    if (!(self.Notification && self.Notification.permission === "granted")) {
+      return;
+    }
 
-        if(!(self.Notification && self.Notification.permission === 'granted')) {
-            return;
-        }
-
+    try {
+      if (event.data) {
+        let data = {};
         try {
-            if (event.data) {
-                let data = {};
-                try {
-                    data = event.data.json(); 
-                } catch (error) {
-                    data = { title: 'Push Notification', body: event.data.text(), icon: 'images/icon.png', primaryKey: '1' };
-                }
-
-                const options = {
-                    body: data.body || 'Você tem uma nova notificação.',
-                    icon: data.icon || 'images/icon.png',
-                    vibrate: [100, 50, 100],
-                    data: {
-                        dateOfArrival: Date.now(),
-                        primaryKey: data.primaryKey || '1'
-                    }
-                };
-
-                event.waitUntil(
-                    self.registration.showNotification(data.title || 'Push Notification', options)
-                );
-            } else {
-                const fallbackOptions = {
-                    body: 'Você tem uma nova notificação.',
-                    icon: 'images/icon.png',
-                    vibrate: [100, 50, 100],
-                    data: {
-                        dateOfArrival: Date.now(),
-                        primaryKey: '2'
-                    }
-                };
-                event.waitUntil(
-                    self.registration.showNotification('Push Notification', fallbackOptions)
-                );
-            }
+          data = event.data.json();
         } catch (error) {
-            console.error('Error handling push event:', error);
+          data = {
+            title: "Push Notification",
+            body: event.data.text(),
+            icon: "images/icon.png",
+            primaryKey: "1",
+          };
         }
-    };
 
-    getData(event);
+        console.log("data", data);
+
+        const options = {
+          body: data.body || "Você tem uma nova notificação.",
+          icon: data.icon || "images/icon.png",
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: data.primaryKey || "1",
+          },
+        };
+
+        event.waitUntil(
+          self.registration.showNotification(
+            data.title || "Push Notification",
+            data
+          )
+        );
+      } else {
+        const fallbackOptions = {
+          body: "Você tem uma nova notificação.",
+          icon: "images/icon.png",
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: "2",
+          },
+        };
+        event.waitUntil(
+          self.registration.showNotification(
+            "Push Notification",
+            fallbackOptions
+          )
+        );
+      }
+    } catch (error) {
+      console.error("Error handling push event:", error);
+    }
+  };
+
+  getData(event);
 });
 
-
-
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://example.com')
-    );
-
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  console.log("notificationclick", event.notification.data);
+  event.waitUntil(
+    clients.openWindow(window.location.origin + "/morador/perfil")
+  );
 });
