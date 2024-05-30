0da30a6e6fdf5f190d07cf89bb804d830794bfef
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-30 14:54:09 -0300

    refactor: Rename SubscripitionDTO to SubscriptionDTO

--- Back-end/src/main/java/com/CondoSync/services/ApiPushManagerService.java
+++ Back-end/src/main/java/com/CondoSync/services/ApiPushManagerService.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.CondoSync.models.DTOs.SubscripitionDTO;
+import com.CondoSync.models.DTOs.SubscriptionDTO;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -19,53 +19,29 @@ public class ApiPushManagerService {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-    public Mono<String> getDataFromExternalApi() {
-        return webClientBuilder.build()
-                .get()
-                .uri("http://external-api.com/data")
-                .retrieve()
-                .bodyToMono(String.class);
-    }
-
-    public Mono<String> postDataToExternalApi(Object requestBody) {
-        return webClientBuilder.build()
-                .post()
-                .uri("http://external-api.com/data")
-                .body(Mono.just(requestBody), Object.class)
-                .retrieve()
-                .bodyToMono(String.class);
-    }
-
     @Async
-    public void sendNotification(List<SubscripitionDTO> subscriptions, Payload payload) {
+    public void sendNotification(List<SubscriptionDTO> subscriptions, Payload payload) {
         log.info("Enviando notificação para: " + subscriptions.size() + " dispositivos.");
         try {
-
-            InnerApiPushManagerService innerApiPushManagerService = new InnerApiPushManagerService(subscriptions,
-                    payload);
-
             webClientBuilder.build()
                     .post()
                     .uri("http://localhost:8020/api/v1/notifications/send-notification")
-                    .body(Mono.just(subscriptions), List.class)
+                    .body(Mono.just(new NotificationRequest(subscriptions, payload)), NotificationRequest.class)
                     .retrieve()
                     .bodyToMono(String.class)
                     .doOnNext(response -> {
                         log.info("Notificação enviada com sucesso: " + response);
                     })
                     .doOnError(error -> {
-
-                        log.error("Erro ao enviar notificação: " + error.getMessage());
+                        log.error("Erro ao enviar notificação: ", error);
                     })
                     .subscribe();
         } catch (Exception e) {
-            log.error("Erro ao enviar notificação: " + e.getMessage());
+            log.error("Erro ao enviar notificação: ", e);
         }
-
     }
 
     public static class Payload {
-
         String title;
         String body;
         String icon;
@@ -74,18 +50,21 @@ public class ApiPushManagerService {
         String tag;
         String url;
         List<Action> actions;
-
     }
 
     public static class Action {
-
         String action;
         String title;
         String icon;
-
     }
 
-    public record InnerApiPushManagerService(List<SubscripitionDTO> subscripitionDTOs, Payload payload) {
-    }
+    public static class NotificationRequest {
+        List<SubscriptionDTO> subscriptions;
+        Payload payload;
 
+        public NotificationRequest(List<SubscriptionDTO> subscriptions, Payload payload) {
+            this.subscriptions = subscriptions;
+            this.payload = payload;
+        }
+    }
 }
