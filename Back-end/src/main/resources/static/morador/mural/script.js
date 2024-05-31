16b3441f8a6cd0f6599b0e0e1a2764319d2f1491
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-05-31 20:07:25 -0300

    refactor: Update mural script to fetch and display data
    The code changes in script.js update the `getDadosMural` function to fetch data from the server and display it in the mural carousel. It also adds a loading indicator while the data is being fetched and handles error cases. This refactor improves the functionality and user experience of the mural feature.

--- Back-end/src/main/resources/static/morador/mural/script.js
+++ Back-end/src/main/resources/static/morador/mural/script.js
@@ -4,7 +4,16 @@ const baseUrl = "https://condosyn.eastus.cloudapp.azure.com:4433";
 
 document.addEventListener("DOMContentLoaded", () => {
   token.validateSecurity();
+  getDadosMural();
 });
+
+function showLoading() {
+  document.getElementById("loading").style.display = "block";
+}
+
+function hideLoading() {
+  document.getElementById("loading").style.display = "none";
+}
 
 function showToast(titulo, message, clss = "bg-primary", time = 5000) {
   const toastContainer = document.getElementById("toastContainer");
@@ -39,3 +48,102 @@ function showToast(titulo, message, clss = "bg-primary", time = 5000) {
 }
 
 document.getElementById("btn-logout").addEventListener("click", token.logout);
+
+async function getDadosMural() {
+  token.validateSecurity();
+  showLoading();
+
+  try {
+    const response = await fetch(`${baseUrl}/api/v1/mural/lists`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token.getToken(),
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      if (data.length === 0) {
+        buildMural(dataUnicoAviso, true);
+        return;
+      }
+
+      buildMural(data);
+    } else {
+      buildMural(dataUnicoAviso, true);
+    }
+  } catch (error) {
+    buildMural(dataUnicoAviso, true);
+    console.error("Error:", error);
+  } finally {
+    hideLoading();
+  }
+}
+
+const dataUnicoAviso = [
+  {
+    title: "Sem avisos no momento!",
+    description: "Nenhum aviso foi cadastrado no momento.",
+    image: {
+      base64: "/imagens/imagensMural/mural-de-avisos_thumb2.png",
+    },
+  },
+];
+
+function buildMural(data, unicoAviso = false) {
+  const muralCaroussel = document.getElementById("carouselMural");
+  const muralIndicators = document.getElementById("carouselMural-indicators");
+  const muralItems = document.getElementById("carouselMural-items");
+
+  data.forEach((item, index) => {
+    const indicator = document.createElement("button");
+    indicator.type = "button";
+    indicator.dataset.bsTarget = "#carouselMural";
+    indicator.dataset.bsSlideTo = index;
+    if (index === 0) {
+      indicator.classList.add("active");
+      indicator.setAttribute("aria-current", "true");
+    }
+    indicator.setAttribute("aria-label", `Slide ${index + 1}`);
+    muralIndicators.appendChild(indicator);
+
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("carousel-item");
+    if (index === 0) {
+      itemDiv.classList.add("active");
+    }
+    const img = document.createElement("img");
+    img.src = item.image.base64;
+    img.classList.add("d-block", "w-auto", "img-fluid", "rounded", "mx-auto");
+    itemDiv.appendChild(img);
+
+    if (!unicoAviso) {
+      const carouselCaption = document.createElement("div");
+      carouselCaption.classList.add(
+        "carousel-caption",
+        "d-none",
+        "d-md-block",
+        "bg-dark",
+        "text-white",
+        "rounded",
+        "mx-auto",
+        "m-3"
+      );
+      carouselCaption.style.opacity = 0.7;
+
+      const h5 = document.createElement("h4");
+      h5.textContent = item.title;
+      carouselCaption.appendChild(h5);
+
+      if (item.description) {
+        const p = document.createElement("p");
+        p.textContent = item.description;
+        carouselCaption.appendChild(p);
+      }
+    }
+
+    muralItems.appendChild(itemDiv);
+  });
+}
