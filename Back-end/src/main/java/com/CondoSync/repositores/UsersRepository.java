aee51321deca8b3281b37bce9c205f5b99ac0c7a
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-06-02 00:03:16 -0300

    refactor: Add lazy loading for user roles in UsersRepository

--- Back-end/src/main/java/com/CondoSync/repositores/UsersRepository.java
+++ Back-end/src/main/java/com/CondoSync/repositores/UsersRepository.java
@@ -1,10 +1,12 @@
 package com.CondoSync.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.CondoSync.models.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,5 +16,8 @@ public interface UsersRepository extends JpaRepository<User, UUID> {
     Optional<User> findByuserName(String username);
 
     boolean existsByuserName(String string);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles r WHERE r.nome = 'ADMIN'")
+    List<User> findAllAdmins();
 
 }
