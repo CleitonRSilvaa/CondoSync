5ebacc5dc6d0cf8ee507665702300b38b5c1f0c3
Cleiton.Silva <cleiton_ribeiro@live.com>
2024-06-02 00:04:55 -0300

    refactor: Update UserController with authorization and error handling
    The code changes in UserController.java add authorization annotations and error handling for various endpoints. The `@PreAuthorize` annotation is used to restrict access to certain endpoints based on user roles. Error handling is implemented for failed registration and updating user status. These refactorings improve the security and user experience of the application.

--- Back-end/src/main/java/com/CondoSync/controllers/UserController.java
+++ Back-end/src/main/java/com/CondoSync/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.CondoSync.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.CondoSync.models.User;
+import com.CondoSync.models.DTOs.ResponseDTO;
+import com.CondoSync.models.DTOs.UsuarioDTO;
 import com.CondoSync.services.UserService;
 
 import jakarta.validation.Valid;
@@ -20,35 +27,114 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getUsers();
+    // @GetMapping
+    // public List<User> getUsers() {
+    // return userService.getUsers();
+    // }
+
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @GetMapping("/list")
+    public ResponseEntity<?> listAll() {
+        var users = userService.ListAll();
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable UUID id) {
-        return userService.getUserById(id);
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+
+        var user = userService.register(usuarioDTO);
+        if (user == null) {
+
+            var responseDTO = new ResponseDTO();
+            responseDTO.setMessage("Erro ao registrar usuário");
+            responseDTO.setError("Erro ao registrar usuário");
+            responseDTO.setStatus(400);
+
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(usuarioDTO, HttpStatus.CREATED);
     }
 
-    @PostMapping
-    public User createUser(@RequestBody @Valid User user) {
-        return userService.createUser(user);
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @GetMapping("/find/{userId}")
+    public ResponseEntity<?> findByEmail(@PathVariable UUID userId) {
+        var user = userService.getUser(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PutMapping
-    public User updateUser(@RequestBody @Valid User user) {
-        return userService.updateUser(user);
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PutMapping("/update/{userId}")
+    public ResponseEntity<?> update(
+            @PathVariable UUID userId,
+            @RequestBody @Valid UsuarioDTO usuarioDTO) {
+
+        var user = userService.updateUser(userId, usuarioDTO);
+        if (user == null) {
+            var responseDTO = new ResponseDTO();
+            responseDTO.setMessage("Erro ao atualizar usuário");
+            responseDTO.setError("Erro ao atualizar usuário");
+            responseDTO.setStatus(400);
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
     }
 
-    @PatchMapping
-    public User patchUser(@RequestBody @Valid User user) {
-        return userService.patchUser(user);
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @PutMapping("/change-status/{userId}")
+    public ResponseEntity<?> changeStatus(
+            @PathVariable UUID userId, JwtAuthenticationToken jwtAuthenticationToken) {
+
+        User userAlth = userService.findByUserName(jwtAuthenticationToken.getToken().getSubject()).orElseThrow(
+                () -> new UsernameNotFoundException(
+                        "Usuário não encontrado: " + jwtAuthenticationToken.getToken().getSubject()));
+
+        System.out.println("userAlth.getId() " + userAlth.getId());
+
+        if (userAlth.getId().equals(userId)) {
+            var responseDTO = new ResponseDTO();
+            responseDTO.setMessage("Usuário não pode alterar seu próprio status");
+            responseDTO.setError("Usuário não pode alterar seu próprio status");
+            responseDTO.setStatus(400);
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+
+        var user = userService.changeStatus(userId);
+        if (user == null) {
+            var responseDTO = new ResponseDTO();
+            responseDTO.setMessage("Erro ao atualizar status do usuário");
+            responseDTO.setError("Erro ao atualizar status do usuário");
+            responseDTO.setStatus(400);
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public void deleteUser(@RequestBody @Valid User user) {
-        userService.deleteUser(user);
+    // @GetMapping("/{id}")
+    // public User getUserById(@PathVariable UUID id) {
+    // return userService.getUserById(id);
+    // }
 
-    }
+    // @PostMapping
+    // public User createUser(@RequestBody @Valid User user) {
+    // return userService.createUser(user);
+    // }
+
+    // @PutMapping
+    // public User updateUser(@RequestBody @Valid User user) {
+    // return userService.updateUser(user);
+    // }
+
+    // @PatchMapping
+    // public User patchUser(@RequestBody @Valid User user) {
+    // return userService.patchUser(user);
+    // }
+
+    // @DeleteMapping
+    // public void deleteUser(@RequestBody @Valid User user) {
+    // userService.deleteUser(user);
+
+    // }
 
 }
